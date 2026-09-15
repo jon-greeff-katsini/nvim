0622858b5ef937a9146d895a7658c771173d6764
Jon Greeff-Katsini <jon@greeff-katsini.co.za>
2026-09-15 12:22:15 +0200

    added html, css and js lsps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,4 +13,29 @@ vim.lsp.config('basedpyright', {
 
 vim.lsp.enable 'basedpyright'
 
+local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+vim.lsp.config('html', {
+  cmd = { 'vscode-html-language-server', '--stdio' },
+  capabilities = capabilities,
+  filetypes = { 'html' },
+  root_markers = { 'package.json', '.git' },
+})
+
+vim.lsp.config('cssls', {
+  cmd = { 'vscode-css-language-server', '--stdio' },
+  capabilities = capabilities,
+  filetypes = { 'css', 'scss', 'less' },
+  root_markers = { 'package.json', '.git' },
+})
+
+vim.lsp.config('ts_ls', {
+  cmd = { 'typescript-language-server', '--stdio' },
+  capabilities = capabilities,
+  filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  root_markers = { 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' },
+})
+
+vim.lsp.enable { 'html', 'cssls', 'ts_ls' }
+
 vim.diagnostic.config { virtual_text = true }
